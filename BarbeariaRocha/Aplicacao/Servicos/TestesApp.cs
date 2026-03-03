ce3ae36acdd456f07213458aa4e7811c426ca3e0
Saulo Gustavo <saulo0100@outlook.com>
2026-03-03 12:55:12 -0300

    Ajuste com valores reais

--- BarbeariaRocha/Aplicacao/Servicos/TestesApp.cs
+++ BarbeariaRocha/Aplicacao/Servicos/TestesApp.cs
@@ -30,7 +30,7 @@ namespace BarbeariaRocha.Aplicacao.Servicos
             // Usuários
             var usuario1 = new Usuario
             {
-                Nome = "Cliente Teste 1",
+                Nome = "Saulo gustavo",
                 Numero = "11987654321",
                 Email = "cliente1@teste.com",
                 Perfil = Perfil.Cliente.ToString(),
@@ -38,7 +38,7 @@ namespace BarbeariaRocha.Aplicacao.Servicos
             };
             var usuario2 = new Usuario
             {
-                Nome = "Barbeiro Teste 1",
+                Nome = "Gerson Lucas",
                 Numero = "11912345678",
                 Email = "barbeiro1@teste.com",
                 Perfil = Perfil.Barbeiro.ToString(),
@@ -47,10 +47,30 @@ namespace BarbeariaRocha.Aplicacao.Servicos
             };
             var usuario3 = new Usuario
             {
-                Nome = "Admin Teste",
+                Nome = "Yago Rocha",
                 Numero = "11999999999",
                 Email = "admin@teste.com",
-                Perfil = Perfil.Administrador.ToString(),
+                Perfil = Perfil.BarbeiroAdministrador.ToString(),
+                Senha = "senha123",
+                Agenda = TipoAgenda.Mensal.ToString()
+            };
+
+            var usuario4 = new Usuario
+            {
+                Nome = "Neymar Junior",
+                Numero = "11999999999",
+                Email = "admin@teste.com",
+                Perfil = Perfil.Barbeiro.ToString(),
+                Senha = "senha123",
+                Agenda = TipoAgenda.Mensal.ToString()
+            };
+
+            var usuario5 = new Usuario
+            {
+                Nome = "Vinicius Junior",
+                Numero = "11999999999",
+                Email = "admin@teste.com",
+                Perfil = Perfil.Barbeiro.ToString(),
                 Senha = "senha123",
                 Agenda = TipoAgenda.Mensal.ToString()
             };
@@ -61,36 +81,137 @@ namespace BarbeariaRocha.Aplicacao.Servicos
             // Serviços
             var servico1 = new Servico
             {
-                Descricao = "Corte de Cabelo",
+                Descricao = "Social",
                 Valor = 30.00m,
                 TempoEstimado = new TimeOnly(0, 40, 0),
                 Excluido = false,
                 Categoria = "Cabelo"
             };
+
             var servico2 = new Servico
             {
-                Descricao = "Corte de Cabelo e Barba",
-                Valor = 50.00m,
+                Descricao = "Degrade",
+                Valor = 35.00m,
                 TempoEstimado = new TimeOnly(0, 40, 0),
                 Excluido = false,
                 Categoria = "Cabelo"
             };
+
             var servico3 = new Servico
             {
+                Descricao = "Navalhado ou Shaver",
+                Valor = 40.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico4 = new Servico
+            {
+                Descricao = "Cabelo e Barba",
+                Valor = 60.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico5 = new Servico
+            {
                 Descricao = "Barba",
-                Valor = 25.00m,
-                TempoEstimado = new TimeOnly(0, 20, 0),
+                Valor = 30.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
                 Excluido = false,
                 Categoria = "Barba"
             };
-            _contexto.Servico.AddRange(servico1, servico2);
+
+            var servico6 = new Servico
+            {
+                Descricao = "Sobrancelha",
+                Valor = 5.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Sombrancelha"
+            };
+
+            var servico7 = new Servico
+            {
+                Descricao = "Luzes com Corte",
+                Valor = 150.00m,
+                TempoEstimado = new TimeOnly(1, 20, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico8 = new Servico
+            {
+                Descricao = "Luzes sem Corte",
+                Valor = 120.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico9 = new Servico //Esse serviço precisa ser feito em 2 horarios com diferença minima de 4 horas entre eles
+            {
+                Descricao = "Platinado com Corte",
+                Valor = 150.00m,
+                TempoEstimado = new TimeOnly(1, 20, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico10 = new Servico
+            {
+                Descricao = "Platinado sem Corte",
+                Valor = 120.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico11 = new Servico
+            {
+                Descricao = "Alisamento Americano",
+                Valor = 25.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            var servico12 = new Servico
+            {
+                Descricao = "Hidratação",
+                Valor = 20.00m,
+                TempoEstimado = new TimeOnly(0, 40, 0),
+                Excluido = false,
+                Categoria = "Cabelo"
+            };
+
+            //var servico13 = new Servico
+            //{
+            //    Descricao = "Progressiva com Corte",
+            //    Valor = 100.00m,
+            //    TempoEstimado = new TimeOnly(0, 60, 0),
+            //    Excluido = false,
+            //    Categoria = "Cabelo"
+            //};
+
+            //var servico14 = new Servico
+            //{
+            //    Descricao = "Progressiva sem Corte",
+            //    Valor = 70.00m,
+            //    TempoEstimado = new TimeOnly(0, 60, 0),
+            //    Excluido = false,
+            //    Categoria = "Cabelo"
+            //};
+            _contexto.Servico.AddRange(servico1, servico2, servico3, servico4, servico5, servico6, servico7);
             _contexto.SaveChanges();
 
             var amanha = DateTime.Today.AddDays(1);
 
             var horarios = new List<DateTime>
             {
-                new DateTime(amanha.Year, amanha.Month, amanha.Day, 9, 20, 0),
+                new DateTime(amanha.Year, amanha.Month, amanha.Day, 16, 00, 0),
                 new DateTime(amanha.Year, amanha.Month, amanha.Day, 10, 0, 0),
                 new DateTime(amanha.Year, amanha.Month, amanha.Day, 10, 40, 0)
             };
