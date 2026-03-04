eb9d49d558636c93a7869f8fb9378ca15a1a0ea3
Saulo Gustavo <saulo0100@outlook.com>
2026-03-03 21:32:16 -0300

    Migration

--- BarbeariaRocha/Migrations/20260304002247_posDevin.Designer.cs
+++ BarbeariaRocha/Migrations/20260304002247_posDevin.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BarbeariaRocha.Infraestrutura.Contexto;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BarbeariaRocha.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20260304002247_posDevin")]
+    partial class posDevin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
