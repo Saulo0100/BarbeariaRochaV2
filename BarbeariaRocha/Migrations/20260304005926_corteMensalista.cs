580c7028974dafde3503be3b1f6904a5f6e0b2fa
Saulo Gustavo <saulo0100@outlook.com>
2026-03-03 22:08:42 -0300

    adicionar migartion

---
+++ BarbeariaRocha/Migrations/20260304005926_corteMensalista.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BarbeariaRocha.Migrations
+{
+    /// <inheritdoc />
+    public partial class corteMensalista : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MensalistaCortes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MensalistaId = table.Column<int>(type: "integer", nullable: false),
+                    DataCorte = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Observacao = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MensalistaCortes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MensalistaCortes_Mensalistas_MensalistaId",
+                        column: x => x.MensalistaId,
+                        principalTable: "Mensalistas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MensalistaCortes_MensalistaId",
+                table: "MensalistaCortes",
+                column: "MensalistaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MensalistaCortes");
+        }
+    }
+}
