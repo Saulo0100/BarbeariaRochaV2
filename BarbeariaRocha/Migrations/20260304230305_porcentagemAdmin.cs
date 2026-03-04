c45981e889a1d4ada40d4cc0dee1a5f63acf4918
Saulo Gustavo <saulo0100@outlook.com>
2026-03-04 20:25:29 -0300

    migration

---
+++ BarbeariaRocha/Migrations/20260304230305_porcentagemAdmin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarbeariaRocha.Migrations
+{
+    /// <inheritdoc />
+    public partial class porcentagemAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PorcentagemAdminNaEpoca",
+                table: "Agendamentos",
+                type: "numeric",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PorcentagemAdminNaEpoca",
+                table: "Agendamentos");
+        }
+    }
+}
