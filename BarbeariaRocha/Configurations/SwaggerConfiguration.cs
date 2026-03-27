d76aaa11b595015adc63963a7c135dab62c6d469
Saulo Gustavo <70772825+Saulo0100@users.noreply.github.com>
2026-03-27 18:23:49 -0300

    teste de restart

--- BarbeariaRocha/Configurations/SwaggerConfiguration.cs
+++ BarbeariaRocha/Configurations/SwaggerConfiguration.cs
@@ -8,7 +8,7 @@ namespace BarbeariaRocha.Configurations
         {
             services.AddSwaggerGen(static c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo
+                c.SwaggerDoc("v2", new OpenApiInfo
                 {
                     Title = "Barbearia do rocha API"
                 });
